03e1c16bc47fc646ca434ff3886a3cc790ea739b
Guillaume Valadon <guillaume@valadon.net>
2020-04-22 22:38:58 +0200

    Strip a binary using the --target argument

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
-// Copyright (C) 2019 Guillaume Valadon <guillaume@valadon.net>
+// Copyright (C) 2020 Guillaume Valadon <guillaume@valadon.net>
 
-use cargo_metadata::MetadataCommand;
 use std::fs;
 use std::fs::File;
 use std::io::ErrorKind;
 use std::path::PathBuf;
 use std::process::{Command, Stdio};
+
+use cargo_metadata::MetadataCommand;
+use clap::{crate_version, App, Arg};
 
 fn strip_binary(filepath: &mut PathBuf) -> Result<(), String> {
     // Retrieve files metadata
@@ -66,6 +68,18 @@ fn strip_binary(filepath: &mut PathBuf) -> Result<(), String> {
 }
 
 fn main() -> Result<(), String> {
+    // Parse command line arguments
+    let matches = App::new("cargo-strip - reduces the size of binaries using the `strip` command")
+        .version(&crate_version!()[..])
+        .arg(
+            Arg::with_name("target")
+                .short("t")
+                .long("target")
+                .takes_value(true)
+                .help("name of the target to strip"),
+        )
+        .get_matches();
+
     // Check if the strip binary is available
     Command::new("strip")
         .stdout(Stdio::null())
@@ -102,8 +116,27 @@ fn main() -> Result<(), String> {
             .filter(|t| t.kind == vec!["bin"])
             .map(|x| &x.name)
         {
+            // Build a path containing the specified target
+            let mut path = if matches.is_present("target") {
+                let path = entry.path();
+                // Remove the last element from the path
+                let root_path = path
+                    .ancestors()
+                    .nth(1)
+                    .ok_or("Cannot remove build from path!")?;
+
+                // Add the target & the last element of the path
+                root_path.join(matches.value_of("target").unwrap()).join(
+                    path.components()
+                        .last()
+                        .ok_or("Cannot extract build from path!")?
+                        .as_os_str(),
+                )
+            } else {
+                entry.path()
+            };
+
             // Check if the binary exists in the current directory
-            let mut path = entry.path();
             path.push(binary);
             if !path.is_file() {
                 continue;
